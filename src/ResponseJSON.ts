6e9ea590f0fe2ab0c43bace3e3d425ad9f49e8b7
scippio <scippio@2ants.eu>
2017-02-20 09:15:00 +0100

    JSON response stringify only before get data

--- src/ResponseJSON.ts
+++ src/ResponseJSON.ts
@@ -8,9 +8,9 @@ export class ResponseJSON extends Response {
 
         if(!Lodash.isUndefined(statusCodeOrData)){
             if(Lodash.isNumber(statusCodeOrData)) this._statusCode = statusCodeOrData
-            else this._data = JSON.stringify(statusCodeOrData)
+            else this._raw = data // this._data = JSON.stringify(statusCodeOrData)
         }
-        if(data !== undefined) this._data = JSON.stringify(data)
+        if(data !== undefined) this._raw = data //this._data = JSON.stringify(data)
 
         this._headers.set("Content-Type","application/json")
     }
@@ -18,13 +18,19 @@ export class ResponseJSON extends Response {
     //
 
     get data(): string | any {
-        return this._data // always string
+        // return this._data // always string
+        return JSON.stringify(this._raw)
     }
 
     //
 
-    set data(data: any | string){
-        if(Lodash.isString(data)) this._data = data
-        else this._data = JSON.stringify(data)
+    set data(data: any){ // | string
+        //if(Lodash.isString(data)) this._data = data
+        //else this._data = JSON.stringify(data)
+
+        this._raw = data
+
+        // if(Lodash.isString(data)) this._data = data
+        // else this._data = JSON.stringify(data)
     }
 }
